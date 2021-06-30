7aee96d16b733d982e01566f4a6e69ca6c611079
magolol <cragboi@outlook.com>
2021-06-30 17:33:54 -0500

    some changes

--- src/Commands/Images/dumbass.ts
+++ src/Commands/Images/dumbass.ts
@@ -3,12 +3,12 @@ import { Command } from '../../Interfaces';
 
 export const command: Command = {
     name: "dumbass",
-    description: "maurice moment",
+    description: "dumbass moment",
     example: 'c.dumbass',
     public: true,
     aliases: [],
     run: async (client, msg, args) => {
-        const e = new MessageEmbed({ title: "maurice moment" });
+        const e = new MessageEmbed({ title: "dumbass moment" });
         e.setFooter(`Command used by ${msg.author.username}`, msg.author.avatarURL());
         e.setImage('https://media.discordapp.net/attachments/750513295441788979/803341526784016405/image0.jpg');
         msg.channel.send({ embed: e });
