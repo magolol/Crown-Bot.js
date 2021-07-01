f6dc100f4153deabb3ebc44cef7957d36e1b345d
W1kipedia <jrizzo2007@gmail.com>
2021-06-30 21:35:26 -0500

    removed database-controlled commands

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -6,18 +6,11 @@ import { Command, Events, Config, TempConfig } from '../Interfaces';
 import { createConnection } from "mysql";
 import ConfigJson from '../../data/config.json';
 
-class ExtendedClient extends Client{
+class ExtendedClient extends Client {
     public commands: Collection<string, Command> = new Collection();
     public events: Collection<string, Events> = new Collection();
-    public cooldowns = {
-        open_url: [],
-        beg: [],
-        gambleflip: [],
-        rob: [],
-        sponsor: []
-    };
+    public cooldowns = {};
     public tempConfig: TempConfig = {
-        agreedToOpenUrl: false,
         summaryPosition: []
     }
     public config: Config = ConfigJson;
@@ -53,34 +46,6 @@ class ExtendedClient extends Client{
             this.on(event.name, event.run.bind(null, this))
         });
 
-    }
-    public OpenAccount(user_id:string|number): void {
-        const amogus = createConnection(this.config.dbEconomy);
-
-        amogus.connect((err) => {
-            if (err) throw err;
-            const sql = `SELECT client_id FROM Economy WHERE client_id = '${user_id}'`;
-            amogus.query(sql, (err, result: any[]) => {
-                if (err) throw err;
-                if (result.length === 0) {
-                    const queue = `INSERT INTO Economy VALUES ('${user_id}', 69, 420)`;
-                    amogus.query(queue, (problem, yeet) => {if (problem) throw problem;console.log(yeet)});
-                }
-            });
-        });
-    }
-    public async update_bank(user_id:string|number, change:number, mode?: 'bank'|'wallet'): Promise<void> {
-        if (typeof mode === 'undefined'|| mode === null) mode = 'wallet';
-        const con = createConnection(this.config.dbEconomy);
-
-        con.connect((f) => {
-            if (f) throw f;
-            const sql = `UPDATE Economy SET ${mode} = ${mode} + ${change} WHERE client_id = '${user_id}'`;
-            con.query(sql, (err, result) => {
-                if (err) throw err;
-                con.destroy();
-            });
-        });
     }
 
     private async FileExists(fileName: string): Promise<boolean> {
--- src/Interfaces/TempConfig.ts
+++ src/Interfaces/TempConfig.ts
@@ -10,6 +10,5 @@ type MessageInfo = {
 }
 
 export interface TempConfig {
-	agreedToOpenUrl: boolean,
 	summaryPosition?: MessageInfo[]
 }
