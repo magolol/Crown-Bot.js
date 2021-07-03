7b286a330e775a9c3ccd61ae2fc979a84a2e166d
W1kipedia <jrizzo2007@gmail.com>
2021-07-03 01:34:45 -0500

    Remove database types

--- src/Interfaces/Config.ts
+++ src/Interfaces/Config.ts
@@ -1,20 +1,4 @@
 export interface Config {
     token: string,
     prefix: string,
-    dbEconomy: {
-        host:string,
-        database:string,
-        user:string,
-        password:string
-    },
-    dbAll: {
-        host: string,
-        database: string,
-        user: string,
-        password: string
-    },
-    id: {
-        owner : number,
-        guestRole: number
-    }
 }
